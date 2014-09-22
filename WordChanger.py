92f1d3877ebbc7e36f86bfaf8de5e7b27d86eb7f
Per Starke <per@starke-team.de>
2014-09-22 21:48:02 +0200

    Overworked change_method, now uses list (detects, but doesn't change by now)

--- WordChanger.py
+++ WordChanger.py
@@ -1,22 +1,22 @@
+#
+# def wordsToList (string_to_change):
+#     list_with_words = []
+#     i = 0
+#     x = 0
+#     # print("Lenght of String: ", len(string_to_change))
+#     for symbol in string_to_change :
+#         i+=1
+#         if symbol == " " or symbol == "." or symbol ==":" or symbol == "," or symbol =="-" or symbol == "!":
+#             single_word = string_to_change[x:i]
+#             list_with_words.append(single_word)
+#             single_word = ""
+#             x = i
+#
+#     return list_with_words
 
-def wordsToList (string_to_change):
-    list_with_words = []
-    i = 0
-    x = 0
-    # print("Lenght of String: ", len(string_to_change))
-    for symbol in string_to_change :
-        i+=1
-        if symbol == " " or symbol == "." or symbol ==":" or symbol == "," or symbol =="-" or symbol == "!":
-            single_word = string_to_change[x:i]
-            list_with_words.append(single_word)
-            single_word = ""
-            x = i
-
-    return list_with_words
-
-words = ["ironisch" , "erotisch", "Ironisch", "Erotisch", "aggressiv", "attraktiv","Aggressiv", "Atrakktiv",
+words = ["ironisch" , "erotisch", "Ironisch", "Erotisch", "aggressiv", "attraktiv","Aggressiv", "Attrakktiv",
                            "mystisch", "ministerisch", "Mystisch", "Ministerisch", "Ironie", "Erotik", "Aggression", "Attraktivität",
-                           "Ministerium", "Mysterium" "Bundestag", "Schützenverein", "Problem", "Ekzem"]
+                           "Ministerium", "Mysterium", "Bundestag", "Schützenverein", "Problem", "Ekzem"]
 
 print(len(words))
 dict_of_words_to_change = {"ironisch" : "erotisch", "Ironisch":"Erotisch", "aggressiv":"attraktiv","Aggressiv":"Atrakktiv",
@@ -26,49 +26,36 @@ dict_of_words_to_change = {"ironisch" : "erotisch", "Ironisch":"Erotisch", "aggr
                            "ministerisch":"mysterisch", "Ministerisch":"Mysterisch", "Erotik":"Ironie", "Attraktivität":"Aggression",
                            "Mysterium":"Ministerium", "Bundestag" : "Schützenverein", "Schützenverein":"Bundestag", "Problem":"Ekzem",
                            "Ekzem":"Problem"}
-print(len(dict_of_words_to_change))
+# print(len(dict_of_words_to_change))
 
 # word_to_change_to = ""
 
 
-def changeWords():
-    counter = 0
-    for word in list_with_words:
-        # print (word)
-        if word in dict_of_words_to_change:
-            # print(word, " : ", dict_of_words_to_change[word])
-            list_with_words[counter] = dict_of_words_to_change[word]
+
+def changeWordsWithList(string_that_changes):
+    for word in words:
+        if word in string_that_changes:
+            print("\n")
+            print("Change!")
+            print("\n")
+            print(word)
+            print(dict_of_words_to_change[word])
+            
+            string_that_changes.replace(word, dict_of_words_to_change[word])
+    return string_that_changes
 
 
-        elif word[0:len(word)-1] in dict_of_words_to_change:
+string_to_change = "Das ironische Ministerium des Bundestages."     # unbedingt "." oder " " am Ende des Satzes!
+# list_with_words = (wordsToList(string_to_change))
+# print(list_with_words)
+# print ("\n")
+#
 
-            changed_word = word[0:len(word)-1]
-            list_with_words[counter] = dict_of_words_to_change[changed_word] + word[-1]
-
-        elif word[0:len(word)-2] in dict_of_words_to_change:
-            changed_word = word[0:len(word)-2]
-            list_with_words[counter] = dict_of_words_to_change[changed_word] + word[-2:len(word)]
-
-        elif word[0:len(word)-3] in dict_of_words_to_change:
-            changed_word = word[0:len(word)-3]
-            list_with_words[counter] = dict_of_words_to_change[changed_word] + word[-3:len(word)]
-
-        elif word[0:len(word)-4] in dict_of_words_to_change:
-            changed_word = word[0:len(word)-4]
-            list_with_words[counter] = dict_of_words_to_change[changed_word] + word[-4:len(word)]
+print("\n \n")
+# print(" ".join(list_with_words))
+# print("\n")
+# print(changeWords())
 
 
-        counter = counter + 1
-    return " ".join(list_with_words)
-
-
-
-list_with_words = (wordsToList("Das ironische Ministerium des Bundestages.")) # unbedingt "." oder " " am Ende des Satzes!
-print(list_with_words)
-print ("\n")
-
-
-print("\n \n \n")
-print(" ".join(list_with_words))
-print("\n")
-print(changeWords())
+changed_string = changeWordsWithList(string_to_change)
+print(changed_string)
