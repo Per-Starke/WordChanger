9678e9ff3c511f2788135a5e6ec151aa1f890eb0
Per Starke <per@starke-team.de>
2014-09-24 17:15:08 +0200

    Deleted unnecessary comments. Program now doesn't need whitespaces and punctuation marks ( with new Words-list )

--- WordChanger.py
+++ WordChanger.py
@@ -1,67 +1,32 @@
-#
-# def wordsToList (string_to_change):
-#     list_with_words = []
-#     i = 0
-#     x = 0
-#     # print("Lenght of String: ", len(string_to_change))
-#     for symbol in string_to_change :
-#         i+=1
-#         if symbol == " " or symbol == "." or symbol ==":" or symbol == "," or symbol =="-" or symbol == "!":
-#             single_word = string_to_change[x:i]
-#             list_with_words.append(single_word)
-#             single_word = ""
-#             x = i
-#
-#     return list_with_words
+
+
 
 words = ["ironisch", "erotisch", "Ironisch", "Erotisch", "aggressiv", "attraktiv","Aggressiv", "Attrakktiv",
                            "mystisch", "ministerisch", "Mystisch", "Ministerisch", "Ironie", "Erotik", "Aggression", "Attraktivität",
-                           "Ministerium", "Mysterium", "Bundestag", "Schützenverein", "Problem", "Ekzem"]
+                           "Ministerium", "Mysterium", "Bundestag", "Schützenverein", "Problem", "Ekzem"]  # List of Word to changr to scan.
 
-# print(len(words))
-dict_of_words_to_change = {"ironisch" : "erotisch", "Ironisch":"Erotisch", "aggressiv":"attraktiv","Aggressiv":"Atrakktiv",
+
+dict_of_words = {"ironisch" : "erotisch", "Ironisch":"Erotisch", "aggressiv":"attraktiv","Aggressiv":"Atrakktiv",
                            "mystisch":"ministerisch", "Mystisch":"Ministerisch", "Ironie":"Erotik", "Aggression":"Attraktivität",
                            "Ministerium":"Mysterium",
                            "erotisch":"ironisch", "Erotisch":"Ironisch", "attraktiv":"aggressiv", "Attraktiv": "Aggressiv",
                            "ministerisch":"mysterisch", "Ministerisch":"Mysterisch", "Erotik":"Ironie", "Attraktivität":"Aggression",
                            "Mysterium":"Ministerium", "Bundestag" : "Schützenverein", "Schützenverein":"Bundestag", "Problem":"Ekzem",
-                           "Ekzem":"Problem"}
-# print(len(dict_of_words_to_change))
-
-# word_to_change_to = ""
-
+                           "Ekzem":"Problem"}  # dict with the word to change to
 
 
 def changeWords(string_that_changes):
-    newStr = string_that_changes
-    for word in words:
-        if word in string_that_changes:
-            print("\n")
-            print("Change!")
-            print("\n")
-            print(word)
-            print(dict_of_words_to_change[word])
-            newWord = dict_of_words_to_change[word]
-            print("word = ", word)
-            print("newWord = ", newWord)
-            print("newStr = ", newStr)
-            newStr = newStr.replace(word, newWord)
-            print( newStr )
-
+    newStr = string_that_changes  # assignment
+    for word in words:            # scans the list words
+        if word in string_that_changes: # if the word is in the text
+            newWord = dict_of_words[word]
+            newStr = newStr.replace(word, newWord)  # It changes to the assigned word in the dict
     return newStr
 
 
-string_to_change = "Das ironische Ministerium des Bundestages."     # unbedingt "." oder " " am Ende des Satzes!
-# list_with_words = (wordsToList(string_to_change))
-# print(list_with_words)
-# print ("\n")
-#
-
-# print("\n \n")
-# print(" ".join(list_with_words))
-# print("\n")
-# print(changeWords())
+string_to_change = "Des ironischen Ministeriums des Bundestages"  
 
 
 changed_string = changeWords(string_to_change)
-print(changed_string)
+print (string_to_change)
+print (changed_string)
