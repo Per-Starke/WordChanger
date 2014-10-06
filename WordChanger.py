08c0e346733ec5d7e037cfd12c27e99e92d41ce6
Per Starke <per@starke-team.de>
2014-10-06 09:17:53 +0200

    now detects capital letters and small letters and changes them correctly even if list and dict only contain small-letter-words

--- WordChanger.py
+++ WordChanger.py
@@ -1,38 +1,94 @@
 
 
 
-words = ["ironisch", "erotisch", "Ironisch", "Erotisch", "aggressiv", "attraktiv","Aggressiv", "Attrakktiv",
-                           "mystisch", "ministerisch", "Mystisch", "Ministerisch", "Ironie", "Erotik", "Aggression", "Attraktivität",
-                           "Ministerium", "Mysterium", "Bundestag", "Schützenverein", "Problem", "Ekzem"]  # List of Word to changr to scan.
-
-
-# CAPITAL Letters possible?
-
-dict_of_words = {"ironisch" : "erotisch", "Ironisch":"Erotisch", "aggressiv":"attraktiv","Aggressiv":"Atrakktiv",
-                           "mystisch":"ministerisch", "Mystisch":"Ministerisch", "Ironie":"Erotik", "Aggression":"Attraktivität",
-                           "Ministerium":"Mysterium",
-                           "erotisch":"ironisch", "Erotisch":"Ironisch", "attraktiv":"aggressiv", "Attraktiv": "Aggressiv",
-                           "ministerisch":"mysterisch", "Ministerisch":"Mysterisch", "Erotik":"Ironie", "Attraktivität":"Aggression",
-                           "Mysterium":"Ministerium", "Bundestag" : "Schützenverein", "Schützenverein":"Bundestag", "Problem":"Ekzem",
-                           "Ekzem":"Problem"}  # dict with the word to change to
-
-
-def changeWords(string_that_changes):
-    newStr = string_that_changes #.upper(), # assignment
-    for word in words:            # scans the list words
-        if word in string_that_changes: # if the word is in the text
-            newWord = dict_of_words[word]
-            newStr = newStr.replace(word, newWord)  # It changes to the assigned word in the dict
-    return newStr
-
-
-string_to_change = "Des ironischen  ErotikMinisteriums des Bundestages"
-
-
-changed_string = changeWords(string_to_change)
-print (string_to_change)
-print (changed_string)
+words = ["ironisch", "erotisch","aggressiv", "attraktiv",
+                           "mystisch", "ministerisch", "Ironie", "Erotik", "Aggression", "Attraktivität",
+                           "ministerium", "mysterium", "bundestag", "schützenverein", "problem", "ekzem"]  # List of Word to change to scan.
+# every word with small letters
 
 
 
-# List that scans for nouns and sentence-beginns??
+dict_of_words = {"ironisch" : "erotisch","aggressiv":"attraktiv",
+                           "mystisch":"ministerisch", "ironie":"erotik", "aggression":"attraktivität",
+                           "ministerium":"mysterium",
+                           "erotisch":"ironisch", "attraktiv":"aggressiv",
+                           "ministerisch":"mysterisch","erotik":"ironie", "attraktivität":"aggression",
+                           "mysterium":"ministerium", "bundestag" : "schützenverein", "schützenverein":"bundestag", "problem":"ekzem",
+                           "Ekzem":"Problem"}  # dict with the word to change to, every word with small letters
+
+def string_to_list(string_to_change_into_list):
+    list_wiht_chars = []
+    for char in string_to_change_into_list:
+        list_wiht_chars.append(char)
+    return list_wiht_chars
+
+def nested_list_to_string(nested_list):
+    i = 0
+    for list in nested_list:
+        nested_list[i] = "".join(list)
+        i += 1
+    return nested_list
+
+
+def detect_words(string_that_changes):
+    realword = []
+    list_with_detected_words = []
+
+    for word in words:
+        if word.upper() in string_that_changes.upper():
+            nr_1 = string_that_changes.upper().find(word.upper())
+
+            realword = string_to_list(word)
+
+            realword[0] = string_that_changes[nr_1]
+
+            list_with_detected_words.append(realword)
+    return list_with_detected_words
+
+
+def change_words_in_list(list_with_words):
+    list_with_changed_words = []
+    for word in list_with_words:
+        new_word = dict_of_words[word.lower()]
+
+        first = word[0]
+        second = word[0].upper()
+        if first == second:     # if  the first letter is a capital letter
+            new_word = string_to_list(new_word)
+            new_word[0] = new_word[0].upper()
+            "".join(new_word)
+
+
+        list_with_changed_words.append(new_word)
+
+    return list_with_changed_words
+
+
+
+
+def change_words_in_string(list_with_old_words,list_with_new_words, string_that_changes):
+    new_str = string_that_changes
+    i = 0
+    for word in list_with_old_words:
+        new_str = new_str.replace(list_with_old_words[i], list_with_new_words[i])
+        i += 1
+    return new_str
+
+
+def change_words(string_to_change):
+    list = detect_words(string_to_change)
+    old_list = list
+    list = nested_list_to_string(list)
+    list = change_words_in_list(list)
+    list = nested_list_to_string(list)
+    new_list = list
+    string = change_words_in_string(old_list, new_list, string_to_change)
+    return (string)
+
+
+
+string_to_change = "Des ironischen  Erotikministerium des Bundestages"
+
+
+string = change_words(string_to_change)
+print(string)
